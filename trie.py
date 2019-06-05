615f49085df573bc4cba1c0edb9d3239c06b9907
paulogamarra <paulogamarralessa@gmail.com>
2019-06-05 20:42:13 -0300

    trie implementada e descricao do trabalho adicionada

---
+++ trie.py
@@ -0,0 +1,48 @@
+class trie_node(object):
+
+    def __init__(self, char = '*'):
+        self.char = char
+        self.children = []
+        self.data = None
+
+    def add_data(self,key:str,data):
+        node = self
+        for letter in key:
+            found = False
+            for child in node.children:
+                if letter == child.char:
+                    node = child
+                    found = True
+                    break
+            if not found:
+                node.children.append(trie_node(letter))
+                node = node.children[-1]
+        node.data = data
+
+    def get_data(self,key:str):
+        node = self
+        for letter in key:
+            found = False
+            for child in node.children:
+                if letter == child.char:
+                    node = child
+                    found = True
+                    break
+            if not found:
+                return False
+        return node.data
+
+
+
+
+
+if __name__ == '__main__':
+    print('Hewoooooow 0w0')
+    names = trie_node()
+    names.add_data('batata',22)
+    names.add_data('baiacu',71)
+    names.add_data('baiana',38)
+    print(names.get_data('batata'))
+    print(names.get_data('baiana'))
+    print(names.get_data('balada'))
+    print(names.get_data('baiacu'))
