d7e08c18f6224aa842a69fc7d0f5dae1242677dc
paulogamarra <paulogamarralessa@gmail.com>
2019-06-06 21:13:46 -0300

    leitura do arquivo movie.csv agora é feita pelo csv_reader()

--- movie_lens.py
+++ movie_lens.py
@@ -1,16 +1,18 @@
+import csv
 from trie import trie_node
 
 def ids_trie(data_path):
-    with open(data_path, 'r') as movies_csv:
-        lines = movies_csv.readlines()
-
-
     movie_ids = trie_node()
 
-    for line in lines[1:]:
-        movie_ids.add_data(line.split(',')[1][1:-1],int(line.split(',')[0]))
+    with open(data_path, 'r') as movies_csv:
+        csv_reader = csv.reader(movies_csv)
+        next(csv_reader, None)
+        for row in csv_reader:
+            movie_ids.add_data(row[1],int(row[0]))
 
     return movie_ids
+
+
 if __name__ == '__main__':
 
     #INICIALIZACAO DAS ESTRUTURAS DE DADOS
